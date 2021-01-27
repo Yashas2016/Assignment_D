e7c479fe562bc0c28fdc71af1ce87038f68433e6
Yashas2016 <48310083+Yashas2016@users.noreply.github.com>
2021-01-27 18:07:39 +0530

    JSAssignment -2 submitted

---
+++ JSasign-2(1).js
@@ -0,0 +1,6 @@
+const clothes=['jacket', 't-shirt'];
+clothes.length=0;
+console.log(clothes[0]);
+
+//JavaScript executes clothes.length = 0, all the items of the array clothes are deleted. clothes[0] is undefined, because clothes array was emptied
+
---
+++ asign_2_sum(2).js
@@ -0,0 +1,7 @@
+var a=[2,4,6];
+var sum=0;
+for(var i=0;i<a.length;i++)
+{
+	sum+=a[i];
+}
+console.log(sum);
