7670ff603f6651225f6d18832fcefa933d5a1b47
Yashas2016 <48310083+Yashas2016@users.noreply.github.com>
2021-01-27 19:31:44 +0530

    JSAssignment-3 submitted

---
+++ JSAssig-1(1).js
@@ -0,0 +1,16 @@
+ //What will the code below output to the console and why ?
+
+console.log(1 + "2" + "2");
+console.log(1 + +"2" + "2");
+console.log(1 + -"1" + "2");
+console.log(+"1" + "1" + "2");
+console.log( "A" - "B" + "2");
+console.log( "A" -"B" + 2);
+
+// + --> acts as concatenation operation in the console.log that's console.log(1+"2") and. console.log(1+  +"2"  +3) --> 1+ concatenate with +"2" --> 1+2 =3 addition perform. 
+
+//console. log("A"-"B"+"2") --> The output will be "NaN2" as in JavaScript if you are trying to use any operator expect "+" on string ( means " - ",        " / ", " * " ) then it will display "NaN". And if you used "+" then it will concatenate two string as "+" means concatenation. So "A"-"B" will be "NaN" and concate(+) 2 will produce "NaN2"
+
+
+
+
